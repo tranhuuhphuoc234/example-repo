ecdd70046ee7d293cbc947991a59ccf65d17926e
phước <phuoc.th18@aptechlearning.edu.vn>
2021-03-27 11:59:57 +0700

    first modify of Phuoc

--- GithubTest/GithubTest/Program.cs
+++ GithubTest/GithubTest/Program.cs
@@ -10,7 +10,7 @@ namespace GithubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word");
+            Console.WriteLine("Hello Word Tadaaaa!!!");
         }
     }
 }
