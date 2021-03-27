76207d1745372d68e15c10fc09b93f92f3e22be1
phước <phuoc.th18@aptechlearning.edu.vn>
2021-03-27 17:00:52 +0700

    add dcm to program

--- GithubTest/GithubTest/Program.cs
+++ GithubTest/GithubTest/Program.cs
@@ -10,7 +10,7 @@ namespace GithubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word Tadaaaa!!!");
+            Console.WriteLine("Hello Word Tadaaaa!!! dcm");
         }
     }
 }
