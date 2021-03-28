6cdd70a69107f6dcd613bfdc072860ca8f8047ce
phước <phuoc.th18@aptechlearning.edu.vn>
2021-03-28 17:25:46 +0700

    testCheckout

--- GithubTest/GithubTest/Program.cs
+++ GithubTest/GithubTest/Program.cs
@@ -11,6 +11,7 @@ namespace GithubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word Tadaaaa!!! dcm");
+            Console.WriteLine("Add");
         }
     }
 }
